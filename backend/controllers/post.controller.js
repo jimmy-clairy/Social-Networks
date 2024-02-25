1a05d719d3b4f38985d6954d54fb07f2bb383e95
Jimmy Clairy <jimmyclairy@gmail.com>
2024-02-25 17:24:16 +0100

    Add post comment

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -12,17 +12,17 @@ module.exports.createOnePost = async (req, res) => {
         await newPost.save()
         res.status(201).json({ message: 'Create one Post', newPost })
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 
 }
 
 module.exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await PostModel.find()
+        const posts = await PostModel.find().sort({ createdAt: -1 }) // Permet d'inversé les posts
         res.status(200).json({ message: 'Get all posts', posts })
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
@@ -31,7 +31,7 @@ module.exports.getOnePost = async (req, res) => {
         const post = await PostModel.findById(req.params.id)
         res.status(200).json({ message: 'Get one post', post })
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
@@ -52,7 +52,7 @@ module.exports.deleteOnePost = async (req, res) => {
 
         res.status(200).json({ message: 'Delete one post' })
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
@@ -73,7 +73,7 @@ module.exports.updateOnePost = async (req, res) => {
 
         res.status(200).json({ message: 'Update one post', postUpdated })
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
@@ -114,7 +114,7 @@ module.exports.likePost = async (req, res) => {
 
         res.status(200).json({ postLiked, userLiked });
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
@@ -154,8 +154,104 @@ module.exports.unlikePost = async (req, res) => {
 
         res.status(200).json({ postLiked, userLiked });
     } catch (error) {
-        res.status(500).json('Problème Serveur')
+        res.status(500).json({ error })
     }
 }
 
 
+module.exports.commentPost = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        if (!ObjectId.isValid(postId)) {
+            return res.status(400).json({ error: `Invalid Post ID: ${postId}` })
+        }
+
+        const postCommented = await PostModel.findByIdAndUpdate(
+            postId,
+            {
+                $push: {
+                    comments: {
+                        commenterId: req.body.commenterId,
+                        commenterPseudo: req.body.commenterPseudo,
+                        text: req.body.text,
+                        timestamp: new Date().getTime()
+                    }
+                }
+            }, { new: true }
+        )
+
+        res.status(200).json({ postCommented });
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+}
+
+module.exports.editCommentPost = async (req, res) => {
+    try {
+        const postId = req.params.id
+        if (!ObjectId.isValid(postId)) {
+            return res.status(400).json({ error: `Invalid Post ID: ${postId}` })
+        }
+
+        const post = await PostModel.findById(postId)
+        if (!post) {
+            return res.status(400).json({ error: `Invalid Post ID: ${postId}` })
+        }
+
+        const { comments } = post
+        const comment = comments.find((comment) => comment.id === req.body.commentId)
+
+        if (!comment) {
+            return res.status(400).json({ error: `Not found Comment` })
+        }
+
+        comment.text = req.body.text
+
+        await post.save()
+
+        res.status(202).json(comment)
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+}
+
+module.exports.deleteCommentPost = async (req, res) => {
+    try {
+        const postId = req.params.id
+        if (!ObjectId.isValid(postId)) {
+            return res.status(400).json({ error: `Invalid Post ID: ${postId}` })
+        }
+        const post = await PostModel.findById(postId)
+        if (!post) {
+            return res.status(400).json({ error: `Invalid Post ID: ${postId}` })
+        }
+
+        /** Soluce 1 */
+        const { comments } = post
+        const otherComments = comments.filter((comment) => comment.id !== req.body.commentId)
+        if (otherComments.length === 0) {
+            return res.status(400).json({ error: `Not found comment` })
+        }
+        post.comments = otherComments
+
+        await post.save()
+
+        /** Soluce 2 */
+        // const post = await PostModel.findByIdAndUpdate(
+        //     postId,
+        //     {
+        //         $pull: {
+        //             comments: {
+        //                 _id: req.body.commentId
+        //             }
+        //         }
+        //     },
+        //     { new: true }
+        // )
+
+        res.status(202).json(post)
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+}
