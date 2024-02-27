c502c4c656462f9eca9a2da52e39589b9c577275
Jimmy Clairy <jimmyclairy@gmail.com>
2024-02-27 14:48:50 +0100

    Add CLIENT_URL

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const { checkUser, requireAuth } = require('./middlewares/auth.middleware')
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
 
-// Création de l'application express    
+// Création de l'application express
 const app = express();
 
 app.use(express.json());
@@ -19,7 +19,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
 
 // Erreurs CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
@@ -29,6 +29,5 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
-// app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
