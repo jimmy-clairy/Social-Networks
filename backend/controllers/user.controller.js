51aa2702fa84b7e93293dda9a1f1725b8c48779e
Jimmy Clairy <jimmyclairy@gmail.com>
2024-02-28 14:18:59 +0100

    fix bug follow and infollow and delete picture profile if not default picture

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/User.model");
+const fsPromises = require('fs').promises;
 const { checkUserId } = require("../utils/check.utils");
 const { uploadPicture } = require("../utils/uploadPicture.utils");
 
@@ -48,10 +49,9 @@ module.exports.updateOneUser = async (req, res) => {
 
 module.exports.deleteOneUser = async (req, res) => {
     try {
+        const userId = req.params.id;
 
-        const userId = req.params.id
-
-        await checkUserId(userId)
+        const user = await checkUserId(userId);
 
         // Remove user from followers and following arrays of other users
         await UserModel.updateMany(
@@ -59,27 +59,32 @@ module.exports.deleteOneUser = async (req, res) => {
             { $pull: { followers: userId, following: userId } }
         );
 
+        // Delete the user's profile picture if it exists and it's not the default picture
+        const isDefaultPicture = user.picture === './uploads/profil/random-user.png';
+        if (!isDefaultPicture && user.picture) {
+            await fsPromises.unlink(`client/public/${user.picture}`);
+        }
+
         // Delete the user
-        await UserModel.findByIdAndDelete(userId)
+        await UserModel.findByIdAndDelete(userId);
 
-        res.status(200).json('User has been deleted')
+        res.status(200).json('User has been deleted');
     } catch (err) {
-        return res.status(500).json({ error: err.message })
+        return res.status(500).json({ error: err.message });
     }
-}
-
+};
 
 module.exports.follow = async (req, res) => {
     try {
         const followerId = req.params.id;
-        const followingId = req.body._id;
-
-        if (followerId === followingId) {
-            return res.status(400).json({ error: `Impossible follower ID: ${followerId} and following ID: ${followingId} same Id` });
-        }
+        const followingId = req.body.id;
 
         await checkUserId(followerId)
         await checkUserId(followingId)
+
+        if (followerId === followingId) {
+            return res.status(400).json({ error: `Cannot follow yourself. Follower ID: ${followerId}, Following ID: ${followingId}` });
+        }
 
         // Add followingId to follower's following list
         const updatedFollower = await UserModel.findByIdAndUpdate(
@@ -97,23 +102,22 @@ module.exports.follow = async (req, res) => {
 
         // Respond with updated follower and following user
         res.status(201).json({ updatedFollower, updatedFollowing });
-    } catch (error) {
-        console.error('Error in follow:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
 module.exports.unfollow = async (req, res) => {
     try {
         const followerId = req.params.id;
-        const followingId = req.body._id;
-
-        if (followerId === followingId) {
-            return res.status(400).json({ error: `Impossible follower ID: ${followerId} and following ID: ${followingId} same Id` });
-        }
+        const followingId = req.body.id;
 
         await checkUserId(followerId)
         await checkUserId(followingId)
+
+        if (followerId === followingId) {
+            return res.status(400).json({ error: `Cannot unfollow yourself. Follower ID: ${followerId}, Following ID: ${followingId}` });
+        }
 
         // Remove followingId from follower's following list
         const updatedFollower = await UserModel.findByIdAndUpdate(
@@ -130,9 +134,8 @@ module.exports.unfollow = async (req, res) => {
         );
 
         // Respond with updated follower and following user
-        res.status(200).json({ updatedFollower, updatedFollowing });
-    } catch (error) {
-        console.error('Error in unfollow:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(201).json({ updatedFollower, updatedFollowing });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
