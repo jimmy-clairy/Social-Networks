7afd788d7cf32f85699213196d5bc8934290320e
Jimmy Clairy <jimmyclairy@gmail.com>
2024-02-28 13:16:07 +0100

    Delete route uploadController.uploadProfil

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const authController = require('../controllers/auth.controller')
 const userController = require('../controllers/user.controller')
-const uploadController = require('../controllers/upload.controller')
 const multer = require("multer")
 const upload = multer()
 
@@ -20,8 +19,5 @@ router.delete("/:id", userController.deleteOneUser)
 // Follow
 router.patch("/follow/:id", userController.follow)
 router.patch("/unfollow/:id", userController.unfollow)
-
-// Upload
-router.post('/upload', upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
