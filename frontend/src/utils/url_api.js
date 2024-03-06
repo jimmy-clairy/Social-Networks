c183b31089c8027417611236b86a64caa32ed447
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-06 11:06:00 +0100

    Created Url Api Path

---
+++ frontend/src/utils/url_api.js
@@ -0,0 +1,6 @@
+const BASE_URL = `http://localhost:5000/`
+
+export const URL_API_LOGIN = `${BASE_URL}api/user/login`
+export const URL_API_SIGNUP = `${BASE_URL}api/user/signup`
+export const URL_API_GET_USERS = `${BASE_URL}api/user`
+export const URL_API_GET_POSTS = `${BASE_URL}api/post`
