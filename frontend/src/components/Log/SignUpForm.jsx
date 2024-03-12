aea7b94909b5c92a68e27a175d77b611bea23a2a
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-12 14:36:26 +0100

    Improve SignUpForm

--- frontend/src/components/Log/SignUpForm.jsx
+++ frontend/src/components/Log/SignUpForm.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import fetchData from "../../utils/fetchData"
-import { URL_API_LOGIN, URL_API_SIGNUP } from "../../utils/url_api"
+import fetchData from "../../utils/fetchData";
+import { URL_API_LOGIN, URL_API_SIGNUP } from "../../utils/url_api";
 
-async function login(user) {
-    const url = URL_API_LOGIN;
+async function apiRequest(url, user) {
     const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -13,75 +12,57 @@ async function login(user) {
     return await fetchData(url, options);
 }
 
-async function signUp(user) {
-    const url = URL_API_SIGNUP;
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
-    return await fetchData(url, options)
-}
-
 export default function SignUpForm() {
-    const [pseudo, setPseudo] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [controlPassword, setControlPassword] = useState('')
-    const [check, setCheck] = useState(false)
+    const [pseudo, setPseudo] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [controlPassword, setControlPassword] = useState('');
+    const [check, setCheck] = useState(false);
     const [errors, setErrors] = useState({
         pseudo: '',
         email: '',
         password: '',
         controlPassword: '',
         check: ''
-    })
+    });
 
     const navigate = useNavigate();
 
     const handleSignUp = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         try {
-            if (password !== controlPassword || !check) {
-                if (password !== controlPassword) {
-                    setErrors({ ...errors, controlPassword: 'Le mot de passe doit etre identique' })
-                }
-                if (!check) {
-                    setErrors({ ...errors, check: 'Veuillez valider les conditions générales' })
-                }
-
+            if (password !== controlPassword) {
+                setErrors({ ...errors, controlPassword: 'Les mots de passe ne correspondent pas' });
+            } else if (!check) {
+                setErrors({ ...errors, check: 'Veuillez accepter les conditions générales' });
             } else {
-                const user = { pseudo, email, password }
+                const user = { pseudo, email, password };
 
-                const data = await signUp(user)
-
-                if (data.errors) {
-                    return setErrors({ ...errors, pseudo: data.errors.pseudo, email: data.errors.email, password: data.errors.password })
+                const signUpData = await apiRequest(URL_API_SIGNUP, user);
+                if (signUpData.errors) {
+                    setErrors({ ...errors, ...signUpData.errors });
+                } else {
+                    const loginData = await apiRequest(URL_API_LOGIN, user);
+                    localStorage.setItem("token", JSON.stringify(loginData.token));
+                    localStorage.setItem("userId", JSON.stringify(loginData.userId));
+                    navigate("/");
                 }
-
-                const userData = await login(user)
-
-                localStorage.setItem("token", JSON.stringify(userData.token));
-                localStorage.setItem("userId", JSON.stringify(userData.userId));
-
-                navigate("/")
             }
-
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
     return (
-        <form action="" onSubmit={handleSignUp} id="sing-up-form">
+        <form onSubmit={handleSignUp} id="sign-up-form">
 
             <label htmlFor="pseudo">Pseudo</label>
             <input
                 id="pseudo"
                 onChange={(e) => { setPseudo(e.target.value), setErrors({ ...errors, pseudo: '' }) }}
                 value={pseudo}
-                autoComplete="pseudo"
-                type="pseudo"
+                autoComplete="username"
+                type="text"
                 name="pseudo"
                 required
             />
@@ -102,9 +83,9 @@ export default function SignUpForm() {
             <label htmlFor="password">Mot de passe</label>
             <input
                 id="password"
-                onChange={(e) => { setPassword(e.target.value), setErrors({ ...errors, controlPassword: '', password: '' }) }}
+                onChange={(e) => { setPassword(e.target.value), setErrors({ ...errors, password: '', controlPassword: '' }) }}
                 value={password}
-                autoComplete="current-password"
+                autoComplete="new-password"
                 type="password"
                 name="password"
                 required
@@ -116,18 +97,28 @@ export default function SignUpForm() {
                 id="controlPassword"
                 onChange={(e) => { setControlPassword(e.target.value), setErrors({ ...errors, controlPassword: '' }) }}
                 value={controlPassword}
-                autoComplete="current-password"
+                autoComplete="new-password"
                 type="password"
                 name="controlPassword"
                 required
             />
             <div className="error">{errors.controlPassword}</div>
 
-            <input type="checkbox" id="terms" onChange={() => { setCheck(!check), setErrors({ ...errors, check: '' }) }} value={check} />
-            <label htmlFor="terms">J'accepte les <a href="#" target="_blank" rel="noopener noreferrer">conditions généroles</a></label>
+            <input
+                type="checkbox"
+                id="terms"
+                onChange={() => { setCheck(!check), setErrors({ ...errors, check: '' }) }}
+                checked={check}
+            />
+            <label htmlFor="terms">
+                J'accepte les{" "}
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    conditions générales
+                </a>
+            </label>
             <div className="error">{errors.check}</div>
 
             <input type="submit" value="Valider inscription" />
         </form>
-    )
+    );
 }
