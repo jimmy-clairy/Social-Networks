826f9612db089bdfffecbe57da36bdf6ab8cfc58
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-15 12:04:45 +0100

    Modi url api

--- frontend/src/utils/url_api.js
+++ frontend/src/utils/url_api.js
@@ -1,6 +1,6 @@
 const BASE_URL = `http://localhost:5000/`
 
-export const URL_API_LOGIN = `${BASE_URL}api/user/login`
-export const URL_API_SIGNUP = `${BASE_URL}api/user/signup`
-export const URL_API_GET_USER = `${BASE_URL}api/user`
-export const URL_API_GET_POSTS = `${BASE_URL}api/post`
+export const URL_API_LOGIN = `${BASE_URL}api/user/login/`
+export const URL_API_SIGNUP = `${BASE_URL}api/user/signup/`
+export const URL_API_GET_USER = `${BASE_URL}api/user/`
+export const URL_API_GET_POSTS = `${BASE_URL}api/post/`
