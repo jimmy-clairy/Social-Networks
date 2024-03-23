abe330c9dfbe3535dbe2ae45c3ab494e02c87cc6
Jimmy Clairy <jimmyclairy@gmail.com>
2024-03-23 16:36:37 +0100

    Add putUser

--- frontend/src/reducers/user.reducer.js
+++ frontend/src/reducers/user.reducer.js
@@ -1,10 +1,12 @@
-import { GET_USER } from "../actions/user.actions";
+import { GET_USER, PUT_USER } from "../actions/user.actions";
 
 const initialState = {};
 
 export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case GET_USER:
+            return action.payload;
+        case PUT_USER:
             return action.payload;
         default:
             return state;
