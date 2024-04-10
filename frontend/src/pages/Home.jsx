a67e8c0f492a766c256bf778344a38315c602aea
Jimmy Clairy <jimmyclairy@gmail.com>
2024-04-10 13:03:33 +0200

    Clean code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import { useSelector } from "react-redux";
 import LeftNav from '../components/LeftNav';
 import Thread from "../components/Thread";
 
 export default function Home() {
-    // const posts = useSelector((state) => state.postReducer)
-    // console.log(posts);
     return (
         <div className="home">
             <LeftNav />
